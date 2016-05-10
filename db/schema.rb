4fc07f412113a20de0a638c83e29f990245a4a18
Matt <matt24ray@gmail.com>
2016-05-09 19:30:14 -0500

    check4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510000520) do
+ActiveRecord::Schema.define(version: 20160510002734) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +39,16 @@ ActiveRecord::Schema.define(version: 20160510000520) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "wikis", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "private"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
